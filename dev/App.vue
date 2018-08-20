1053f598aac8c11935b04059da7a3b70a0a1555e
jelewine <jelewine@gmail.com>
2018-08-20 17:00:25 +0800

    project init

---
+++ dev/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Player></Player>
+  </div>
+</template>
+
+<script>
+import Player from '../src'
+
+export default {
+  components: {
+    Player,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+* Vue-Video
+* Author: jelewine@gmail.com
+* Github: https://github.com/LiberJe/vue-video
+*/
+
+import player from './player'
+
+export default player
