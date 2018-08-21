565cf924d38ef212512565ec22e0158299353ff9
jelewine <jelewine@gmail.com>
2018-08-21 20:04:56 +0800

    update

--- dev/App.vue
+++ dev/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <Player></Player>
+  <div class="container">
+    <Player
+      :options="options"
+      :plugins="plugins"
+    ></Player>
   </div>
 </template>
 
@@ -8,12 +11,36 @@
 import Player from '../src'
 
 export default {
+  data () {
+    return {
+      options: {
+        src: 'https://vjs.zencdn.net/v/oceans.mp4',
+        controls: true,
+        loop: false
+      },
+      plugins: [
+        {
+          name: 'adaptor',
+          create () {
+            console.log(this)
+          }
+        }
+      ]
+    }
+  },
   components: {
     Player,
+  },
+  mounted () {
+    
   }
 }
 </script>
 
 <style scoped>
-
+.container {
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;
+}
 </style>
--- src/player.vue
+++ src/player.vue
@@ -1,13 +1,71 @@
 <template>
-  <div></div>
+  <div class="player-container">
+    <video ref="player" class="player" v-bind="options">
+      <source :src="options.src" />
+    </video>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+    plugins: {
+      type: Array,
+      default: []
+    },
+  },
+  watch: {
+    'playerOptions.volume' (value) {
+      this.$refs.player.volume = value
+    }
+  },
+  data () {
+    return {
+      playerOptions: {
+        src: '',
+        poster: '',
+        controls: true,
+        autoplay: false,
+        loop: false,
+        preload: 'auto',
+        muted: false,
+        width: '100%',
+        height: '100%',
+        volume: '0.5'
+      }
+    }
+  },
+  methods: {
+    install (plugin) {
+      plugin.create()
+    }
+  },
+  created () {
+    this.plugins.map((plugin, index) => this.install(plugin))
+  },
+  mounted () {
+    this.$watch('options', v => {
+      this.playerOptions = v
+    }, {deep: true})
+  }
 }
 </script>
 
 <style scoped>
+.player-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
 
+.player {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: black;
+}
 </style>
