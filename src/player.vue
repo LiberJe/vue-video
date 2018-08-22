6b5b486e49740f8b663d9411fee8dabfd74934a1
jelewine <jelewine@gmail.com>
2018-08-22 19:48:31 +0800

    update babel

--- src/player.vue
+++ src/player.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="player-container">
-    <video ref="player" class="player" v-bind="options">
-      <source :src="options.src" />
-    </video>
+  <div ref="wrapper" class="player-wrapper">
+    <div ref="container" class="player-container">
+      <div class="p p1">1</div>
+      <div class="p p2">2</div>
+      <div class="p p3">3</div>
+      <!-- <video ref="player" class="player" v-bind="options">
+        <source :src="options.src" />
+      </video> -->
+      <triggerLayer>
+        <div class="p p1">1</div>
+        <div class="p p2">2</div>
+        <div class="p p3">3</div>
+      </triggerLayer>
+    </div>
   </div>
 </template>
 
 <script>
+import triggerLayer from "./triggerLayer"
 export default {
   props: {
     options: {
@@ -17,6 +28,9 @@ export default {
       type: Array,
       default: []
     },
+  },
+  components: {
+    triggerLayer,
   },
   watch: {
     'playerOptions.volume' (value) {
@@ -51,11 +65,36 @@ export default {
     this.$watch('options', v => {
       this.playerOptions = v
     }, {deep: true})
+
+    this.$refs.wrapper.addEventListener('click', e=>{
+      console.log(e)
+      e.target.style.display = 'none';
+      let under = document.elementFromPoint(e.pageX, e.pageY)
+      e.target.style.display = '';
+      e.stopPropagation()
+      let ev = document.createEvent("MouseEvents");  
+      ev.initMouseEvent(e.type, true, true, window, 1, e.screenX, e.screenY, e.clientX, e.clientY); 
+      console.log(e.target, under)
+      under.dispatchEvent(ev); 
+    })
+    // document.querySelector('.p3 p').addEventListener('click', e=> {
+    //   // e.stopPropagation()
+    //   console.log(111,e)
+    // })
+
+    // let ev = document.createEvent("HTMLEvents");  
+    // ev.initEvent('click', true, true); 
+    
+    // document.querySelector('.p3 p').dispatchEvent(ev); 
   }
 }
 </script>
 
 <style scoped>
+.player-wrapper {
+  width: 100%;
+  height: 100%;
+}
 .player-container {
   width: 100%;
   height: 100%;
@@ -68,4 +107,24 @@ export default {
   position: absolute;
   background: black;
 }
+
+.p {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+.p1 {
+  z-index: 2;
+}
+
+.p2 {
+  z-index: 3;
+}
+
+.p3 {
+  z-index: 4;
+  /* pointer-events: none; */
+}
+
 </style>
---
+++ src/triggerLayer.vue
@@ -0,0 +1,8 @@
+
+<script>
+export default {
+  render (h) {
+    return <div>123</div>
+  }
+}
+</script>
